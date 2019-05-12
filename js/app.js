52f3d728db1545a32428f48f8650312f3fe0fe89
Nicolas Maclean <nicolas.maclean2326@gmail.com>
2019-05-12 13:56:53 -0500

    set up
    request.onload() doesn't work, I have no idea why though

--- js/app.js
+++ js/app.js
@@ -1,176 +1,64 @@
-//embed frame
+//html elements
 var embedDiv = document.getElementById('embedDiv');
 var embedFrame = document.getElementById('embed');
-
-//divs to change the embed src
-const rockDiv = document.getElementById('rock');
-const bouncyDiv = document.getElementById('bouncy');
-const circlesDiv = document.getElementById('circles');
-const collisionDiv = document.getElementById('collision');
-const colBallsDiv = document.getElementById('colBalls');
-const moleculesDiv = document.getElementById('molecules');
-const circularDiv = document.getElementById('circular');
-const polarDiv = document.getElementById('polar');
-const snakeDiv = document.getElementById('snake');
-const springDiv = document.getElementById('spring');
-const fractalDiv = document.getElementById('fractal');
-const fourierDiv = document.getElementById('fourier');
-const colonizationDiv = document.getElementById('colonization');
-const rayDiv = document.getElementById('ray');
-
 const descriptionTitle = document.getElementById('descriptionTitle');
 const descriptionParagraph = document.getElementById('descriptionParagraph');
+const buttonsDiv = document.getElementById('buttons');
 
 var nEmbed = document.createElement('embed');
-var active = 7;
 
-const c1 = "#333";
-const c2 = "#0C75D6";
+var data;
+var active;
 
-function updateEmbed() {
-    embedDiv.appendChild(nEmbed);
-    embedDiv.removeChild(embedFrame);
-    embedFrame = nEmbed;
-    switch(active){
-        case 0: rockDiv.childNodes[1].style.color = c1; break;
-        case 1: bouncyDiv.childNodes[1].style.color = c1; break;
-        case 2: circlesDiv.childNodes[1].style.color = c1; break;
-        case 3: collisionDiv.childNodes[1].style.color = c1; break;
-        case 4: colBallsDiv.childNodes[1].style.color = c1; break;
-        case 5: moleculesDiv.childNodes[1].style.color = c1; break;
-        case 6: circularDiv.childNodes[1].style.color = c1; break;
-        case 7: polarDiv.childNodes[1].style.color = c1; break;
-        case 8: snakeDiv.childNodes[1].style.color = c1; break;
-        case 9: springDiv.childNodes[1].style.color = c1; break;
-        case 10: fractalDiv.childNodes[1].style.color = c1; break;
-        case 11: fourierDiv.childNodes[1].style.color = c1; break;
-        case 12: colonizationDiv.childNodes[1].style.color = c1; break;
-        case 13: rayDiv.childNodes[1].style.color = c1; break;
+function init(){
+    let url = 'https://nicolasmaclean.github.io/json/embed.json';
+    let request = new XMLHttpRequest();
+
+    request.open('GET', url);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload() = function() {
+        data = request.response;
+        let pages = data.pages;
+        active = data.active;
+        for(let i = 0; i < pages.length; i++){
+            addPage(pages[i]);
+            updateEmbed();
+        }
     }
 }
 
-rockDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/rock-paper-scissors-lizard-spock/"
-    updateEmbed();
-    rockDiv.childNodes[1].style.color = c2;
-    active = 0;
-    descriptionTitle.innerHTML = "Rock, Paper, Scissors, Lizard, Spock";
-});
+function addPage(page){
+    let nButton = document.createElement('div');
+    nButton.id = page.title;
+    nButton.classList.add('button');
+    nButton.addEventListener('click', () => {
+        active = page.number;
+        clicked();
+    });
+    buttonsDiv.appendChild(nButton);
+}
 
-bouncyDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Bouncing-Circles/"
-    updateEmbed();
-    bouncyDiv.childNodes[1].style.color = c2;
-    active = 1;
-    descriptionTitle.innerHTML = "Bouncing Circles";
-});
+function clicked() {
+    let divs = buttonsDiv.childNodes;
+    for(let i = 0; i < divs.length; i++){
+        if(i === active-1){
+            divs[i].classList.add('active');
+        } else {
+            divs[i].classList.remove('active');
+        }
+    }
 
-circlesDiv.addEventListener('click', () => {
+    descriptionTitle.innerHTML = pages[active].title;
+    descriptionParagraph.innerHTML = pages[active].description;
+ 
     nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Circles/"
-    updateEmbed();
-    circlesDiv.childNodes[1].style.color = c2;
-    active = 2;
-    descriptionTitle.innerHTML = "Circle Thing";
-});
+    nEmbed.src = pages.link;
 
-collisionDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Collision-Detection/"
-    updateEmbed();
-    collisionDiv.childNodes[1].style.color = c2;
-    active = 3;
-    descriptionTitle.innerHTML = "Collision Detection";
-});
+    embedDiv.removeChild(embedFrame);
+    embedDiv.appendChild(nEmbed);
+    embedFrame = nEmbed;
+}
 
-colBallsDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Collision-Sim/";
-    updateEmbed();
-    colBallsDiv.childNodes[1].style.color = c2;
-    active = 4;
-    descriptionTitle.innerHTML = "Ball Collision";
-});
-
-moleculesDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Molecules/";
-    updateEmbed();
-    moleculesDiv.childNodes[1].style.color = c2;
-    active = 5;
-    descriptionTitle.innerHTML = "Molecule Collision";
-});
-
-circularDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Circular-Motion/";
-    updateEmbed();
-    circularDiv.childNodes[1].style.color = c2;
-    active = 6;
-    descriptionTitle.innerHTML = "Circular Motion";
-});
-
-polarDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Polar-Graph/";
-    updateEmbed();
-    polarDiv.childNodes[1].style.color = c2;
-    active = 7;
-    descriptionTitle.innerHTML = "Polar Graph";
-});
-
-snakeDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Snake/";
-    updateEmbed();
-    snakeDiv.childNodes[1].style.color = c2;
-    active = 8;
-    descriptionTitle.innerHTML = "Snake";
-});
-
-springDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Spring-Motion/";
-    updateEmbed();
-    springDiv.childNodes[1].style.color = c2;
-    active = 9;
-    descriptionTitle.innerHTML = "Spring Motion";
-});
-
-fractalDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Fractal-Tree/";
-    updateEmbed();
-    fractalDiv.childNodes[1].style.color = c2;
-    active = 10;
-    descriptionTitle.innerHTML = "Fractal Tree";
-});
-
-fourierDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Fourier-Series/";
-    updateEmbed();
-    fourierDiv.childNodes[1].style.color = c2;
-    active = 11;
-    descriptionTitle.innerHTML = "Fourier Series"
-});
-
-colonizationDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/Space-Colonization/";
-    updateEmbed();
-    colonizationDiv.childNodes[1].style.color = c2;
-    active = 12;
-    descriptionTitle.innerHTML = "Space Colonization Tree"
-});
-
-rayDiv.addEventListener('click', () => {
-    nEmbed = document.createElement('embed');
-    nEmbed.src = "https://nicolasmaclean.github.io/2d-Ray-Casting/";
-    updateEmbed();
-    rayDiv.childNodes[1].style.color = c2;
-    active = 12;
-    descriptionTitle.innerHTML = "2d Ray Casting"
-});
+init();
