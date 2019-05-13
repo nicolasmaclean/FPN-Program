94d6b6decfd6da64463c779534286583fa5b6b4c
Nicolas Maclean <nicolas.maclean2326@gmail.com>
2019-05-13 09:35:54 -0500

    json

--- js/app.js
+++ js/app.js
@@ -7,34 +7,102 @@ const buttonsDiv = document.getElementById('buttons');
 
 var nEmbed = document.createElement('embed');
 
-var data;
-var active;
+var data = {
+    active: 0,
+    pages: [
+        {
+            title: "Rock, Paper, Scissors",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/rock-paper-scissors-lizard-spock/",
+            number: 0
+        }, {
+            title: "Bouncing Circles",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Bouncing-Circles/",
+            number: 1
+        }, {
+            title: "Circle Thing",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Circles/",
+            number: 2
+        }, {
+            title: "Collision Detection",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Collision-Detection/",
+            number: 3
+        }, {
+            title: "Ball Collision",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Collision-Sim/",
+            number: 4
+        }, {
+            title: "Molecule Collision",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Molecules/",
+            number: 5
+        }, {
+            title: "Circular Motion",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Circular-Motion/",
+            number: 6
+        }, {
+            title: "Polar Graph",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Polar-Graph/",
+            number: 7
+        }, {
+            title: "Snake",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Snake/",
+            number: 8
+        }, {
+            title: "Spring Motion",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Spring-Motion/",
+            number: 9
+        }, {
+            title: "Fractal Tree",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Fractal-Tree/",
+            number: 10
+        }, {
+            title: "Fourier Series",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Fourier-Series/",
+            number: 11
+        }, {
+            title: "Space Colonization Tree",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/Space-Colonization/",
+            number: 12
+        }, {
+            title: "2d Ray Casting",
+            description: "n/a",
+            link: "https://nicolasmaclean.github.io/2d-Ray-Casting/",
+            number: 13
+        }
+    ]
+};
 
 function init(){
-    let url = 'https://nicolasmaclean.github.io/json/embed.json';
-    let request = new XMLHttpRequest();
-
-    request.open('GET', url);
-    request.responseType = 'json';
-    request.send();
-
-    request.onload() = function() {
-        data = request.response;
-        let pages = data.pages;
-        active = data.active;
-        for(let i = 0; i < pages.length; i++){
-            addPage(pages[i]);
-            updateEmbed();
-        }
+    pages = data.pages
+    for(let i = 0; i < pages.length; i++){
+        addPage(pages[i]);
     }
+    data.active = 1;
+    clicked();
 }
 
 function addPage(page){
     let nButton = document.createElement('div');
     nButton.id = page.title;
     nButton.classList.add('button');
+
+    let nHeader = document.createElement('h4');
+    nHeader.innerHTML = page.title;
+    nButton.appendChild(nHeader);
     nButton.addEventListener('click', () => {
-        active = page.number;
+        data.active = page.number;
         clicked();
     });
     buttonsDiv.appendChild(nButton);
@@ -42,19 +110,20 @@ function addPage(page){
 
 function clicked() {
     let divs = buttonsDiv.childNodes;
-    for(let i = 0; i < divs.length; i++){
-        if(i === active-1){
-            divs[i].classList.add('active');
+    for(let i = 1; i < divs.length; i++){
+        if(i === data.active+1){
+            divs[i].style.color = "#0C75D6";
         } else {
-            divs[i].classList.remove('active');
+            divs[i].style.color = "#333";
         }
     }
 
-    descriptionTitle.innerHTML = pages[active].title;
-    descriptionParagraph.innerHTML = pages[active].description;
+    //active doesn't change
+    descriptionTitle.innerHTML = data.pages[data.active].title;
+    descriptionParagraph.innerHTML = data.pages[data.active].description;
  
     nEmbed = document.createElement('embed');
-    nEmbed.src = pages.link;
+    nEmbed.src = data.pages[data.active].link;
 
     embedDiv.removeChild(embedFrame);
     embedDiv.appendChild(nEmbed);
